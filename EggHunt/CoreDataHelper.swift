4b13ccaab79c862ec9183caf264f3c34887a151b
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-03-22 15:28:09 +0000

    Making good progress with the EggHunt app.

---
+++ EggHunt/CoreDataHelper.swift
@@ -0,0 +1,106 @@
+//
+//  CoreDataHelper.swift
+//  EggHunt
+//
+//  Created by Chris Smith on 22/03/2017.
+//  Copyright © 2017 CDSApps. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+func addAllEggs() {
+    createEgg(name: "Chicken", imageName: "010-chicken")
+    createEgg(name: "Yellow egg", imageName: "yellowEgg")
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+}
+
+
+func createEgg(name: String, imageName: String) {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let egg = Egg(context: context)
+    egg.name = name
+    egg.imageName = imageName
+}
+
+func getAllEggs() -> [Egg] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    do{
+    let eggs = try context.fetch(Egg.fetchRequest()) as! [Egg]
+        
+        if eggs.count == 0 {
+            addAllEggs()
+            return getAllEggs()
+        }
+        
+        return eggs
+        
+    } catch {}
+    return []
+}
+
+
+func getAllCaughtEggs() -> [Egg] {
+   let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let fetchRequest = Egg.fetchRequest() as NSFetchRequest<Egg>
+    fetchRequest.predicate = NSPredicate(format: "caught == %@", true as CVarArg)
+    
+    do {
+    let eggs = try context.fetch(fetchRequest) as [Egg]
+        return eggs
+    } catch {}
+    return []
+}
+
+
+func getAllUncaughtEggs() -> [Egg] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let fetchRequest = Egg.fetchRequest() as NSFetchRequest<Egg>
+    fetchRequest.predicate = NSPredicate(format: "caught == %@", false as CVarArg)
+    
+    do {
+        let eggs = try context.fetch(fetchRequest) as [Egg]
+        return eggs
+    } catch {}
+    return []
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- EggHunt/ViewController.swift
+++ EggHunt/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateCount = 0
+    var eggs : [Egg] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        eggs = getAllEggs()
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+                // Spawn an egg
+                
+                if let coord = self.manager.location?.coordinate {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coord
+                    let randomLat = (Double(arc4random_uniform(200)) - 100.0) / 100000.0
+                    let randomLong = (Double(arc4random_uniform(200)) - 100.0) / 10000.0
+                    annotation.coordinate.latitude += randomLat
+                    annotation.coordinate.longitude += randomLong
+                    self.mapView.addAnnotation(annotation)
+                }
+            })
+            
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 500, 500)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
     }
-
-
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coord, 500, 500)
+            mapView.setRegion(region, animated: false)
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
